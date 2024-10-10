384b4a6419b70109c25cb5c79080c5b83c66a215
KangWookOh <kanguk456@gmail.com>
2024-10-10 11:08:56 +0900

    contextLoads 주석

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -1,13 +1,16 @@
 package org.example.expert;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ExpertApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//
+//class ExpertApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
